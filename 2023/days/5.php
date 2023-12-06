3e725dcb47e6161d8f686dca64ff21bdd865cd3f
Alan Wynn <alan.wynn@friendlydev.com>
2023-12-06 10:44:16 +0000

    remove dead code

--- 2023/days/5.php
+++ 2023/days/5.php
@@ -103,37 +103,6 @@ function part2(string $input): int
         }
     }
 
-    /*for ($i = 0, $iMax = count($seedRanges); $i < $iMax; $i += 2) {
-        $start = $seedRanges[$i];
-        $length = $seedRanges[$i + 1];
-
-        for ($j = 0; $j < $length; $j++) {
-            $seed = $start + $j;
-            $value = $seed;
-
-            foreach (array_keys($maps) as $src) {
-                $value = (function (int $value, array $map): int {
-                    foreach ($map as $line) {
-                        [
-                            $destStart,
-                            $srcStart,
-                            $length,
-                        ] = explode(' ', $line);
-
-                        if ($value >= (int) $srcStart && $value < (int) $srcStart + (int) $length) {
-                            return (int) $destStart + ($value - $srcStart);
-                        }
-                    }
-
-                    return $value;
-                })(
-                    $value, $maps[$src]);
-            }
-
-            $minLocation = min($minLocation, $value);
-        }
-    }*/
-
     return $minLocation;
 }
 
