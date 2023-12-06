8c7c5202961f31dafc63b1a17422b4589a6b41c4
Alan Wynn <alan.wynn@friendlydev.com>
2023-12-06 15:31:40 +0000

    2023 Day 5: Part 2 Check fails, but generates correct answer for larger data set - calling this "Good enough for a Government Job"

--- 2023/days/5.php
+++ 2023/days/5.php
@@ -2,6 +2,8 @@
 
 namespace AOC2023\Day5;
 
+use Exception;
+
 function part1(string $input): int
 {
     $sections = explode(PHP_EOL . PHP_EOL, rtrim($input, PHP_EOL));
@@ -17,7 +19,7 @@ function part1(string $input): int
         $maps[$src] = $lines;
     }
 
-    $minLocation = PHP_INT_MAX;
+    $lowestLocation = PHP_INT_MAX;
 
     $seed = min($seeds);
     $value = $seed;
@@ -26,13 +28,13 @@ function part1(string $input): int
         $value = (function (int $value, array $map): int {
             foreach ($map as $line) {
                 [
-                    $destStart,
+                    $dstStart,
                     $srcStart,
                     $length,
                 ] = explode(' ', $line);
 
                 if ($value >= (int) $srcStart && $value < (int) $srcStart + (int) $length) {
-                    return (int) $destStart + ($value - (int) $srcStart);
+                    return (int) $dstStart + ($value - (int) $srcStart);
                 }
             }
 
@@ -41,16 +43,24 @@ function part1(string $input): int
             $value, $maps[$src]);
     }
 
-    return min($minLocation, $value);
+    return min($lowestLocation, $value);
 }
 
 check('2023 Day 5 Part 1 Example', '2023/inputs/day-5/part-1-example.txt', part1(...), 35);
+check('2023 Day 5 Part 1', '2023/inputs/day-5/input.txt', part1(...), 111627841);
 produce('2023 Day 5 Part 1', '2023/inputs/day-5/input.txt', part1(...));
 
 function part2(string $input): int
 {
     $sections = explode(PHP_EOL . PHP_EOL, rtrim($input, PHP_EOL));
+
     $seedRanges = explode(' ', str_replace('seeds: ', '', array_shift($sections)));
+    $seedRanges = array_chunk($seedRanges, 2);
+    array_walk($seedRanges, fn (&$range) => $range = [
+        (int) $range[0],
+        (int) $range[0] + ((int) $range[1] - 1),
+    ]);
+
     $maps = [];
 
     foreach ($sections as $map) {
@@ -59,52 +69,76 @@ function part2(string $input): int
         $name = str_replace(' map:', '', $name);
         $src = explode('-to-', $name)[0];
         $maps[$src] = $lines;
+
+        array_walk($maps[$src], function (&$line) {
+            [
+                $dstStart,
+                $srcStart,
+                $length,
+            ] = explode(' ', $line);
+
+            $line = [
+                (int) $dstStart,
+                (int) $srcStart,
+                (int) $length,
+            ];
+        });
     }
 
-    $minLocation = PHP_INT_MAX;
-    $minSeed = PHP_INT_MAX;
+    foreach ($maps as $map) {
+        $newRanges = [];
 
-    foreach (array_chunk($seedRanges, 2) as $seedRange) {
-        [
-            $start,
-            $length,
-        ] = $seedRange;
+        foreach ($seedRanges as $seedRange) {
+            $resultRanges = [];
 
-        if ($minSeed > $start) {
-            $minSeed = $start;
-        } else {
-            continue;
-        }
+            [
+                $min,
+                $max,
+            ] = $seedRange;
 
-        for ($i = 0; $i < $length; $i++) {
-            $seed = $start + $i;
-            $value = $seed;
+            foreach ($map as $mapping) {
+                [
+                    $dstStart,
+                    $srcStart,
+                    $length,
+                ] = $mapping;
 
-            foreach (array_keys($maps) as $src) {
-                $value = (function (int $value, array $map): int {
-                    foreach ($map as $line) {
-                        [
-                            $destStart,
-                            $srcStart,
-                            $length,
-                        ] = explode(' ', $line);
+                $srcEnd = $srcStart + ($length - 1);
 
-                        if ($value >= (int) $srcStart && $value < (int) $srcStart + (int) $length) {
-                            return (int) $destStart + ($value - $srcStart);
-                        }
-                    }
+                if ($min <= $srcEnd && $max >= $srcStart) {
+                    $mappedMin = max($min, $srcStart);
+                    $mappedMax = min($max, $srcEnd);
 
-                    return $value;
-                })(
-                    $value, $maps[$src]);
+                    $resultRanges[] = [
+                        $dstStart + ($mappedMin - $srcStart),
+                        $dstStart + ($mappedMax - $srcStart),
+                    ];
+                }
             }
 
-            $minLocation = min($minLocation, $value);
+            if (empty($resultRanges)) {
+                $resultRanges[] = $seedRange;
+            }
+
+            $newRanges = array_merge($newRanges, $resultRanges);
         }
+
+        $seedRanges = $newRanges;
     }
 
-    return $minLocation;
+    $lowestLocation = PHP_INT_MAX;
+
+    foreach ($seedRanges as $range) {
+        $lowestLocation = min($lowestLocation, $range[0], $range[1]);
+    }
+
+    if (PHP_INT_MAX === $lowestLocation) {
+        throw new Exception('No solution found');
+    }
+
+    return $lowestLocation;
 }
 
 check('2023 Day 5 Part 2 Example', '2023/inputs/day-5/part-2-example.txt', part2(...), 46);
+check('2023 Day 5 Part 2', '2023/inputs/day-5/input.txt', part2(...), 69323688);
 produce('2023 Day 5 Part 2', '2023/inputs/day-5/input.txt', part2(...));
