e149ae31361c0d7f0f28c55dcb3f65e8030bf3ce
Alan Wynn <alan.wynn@friendlydev.com>
2023-12-14 15:15:34 +0000

    2023 Day 14: Solution for Parts 1 and 2

--- 2023/days/14.php
+++ 2023/days/14.php
@@ -34,11 +34,60 @@ check('2023 Day 14 Part 1 Example', '2023/inputs/day-14/part-1-example.txt', par
 check('2023 Day 14 Part 1', '2023/inputs/day-14/input.txt', part1(...), 105249);
 produce('2023 Day 14 Part 1', '2023/inputs/day-14/input.txt', part1(...));
 
-// function part2(string $input): int
-// {
-//     //
-// }
+function part2(string $input): int
+{
+    $lines = explode(PHP_EOL, trim($input, PHP_EOL));
 
-// check('2023 Day 14 Part 2 Example', '2023/inputs/day-14/part-2-example.txt', part2(...), null);
-// check('2023 Day 14 Part 2', '2023/inputs/day-14/input.txt', part2(...), 88680);
-// produce('2023 Day 14 Part 2', '2023/inputs/day-14/input.txt', part2(...));
+    $cycleGrid = static function (array $grid): array {
+        foreach (range(1, 4) as $ignored) {
+            $grid = array_map(null, ...array_map('str_split', $grid));
+            $grid = array_map(static fn (array $array): string => implode('', $array), $grid);
+
+            foreach ($grid as $i => $column) {
+                $grid[$i] = implode('#', array_map(static function (string $section): string {
+                    $arr = str_split($section);
+                    rsort($arr);
+
+                    return implode('', $arr);
+                }, explode('#', $column)));
+
+                $grid[$i] = strrev($grid[$i]);
+            }
+        }
+
+        return $grid;
+    };
+
+    $seen = [$lines];
+    $iterations = 0;
+
+    while (true) {
+        $iterations++;
+        $lines = $cycleGrid($lines);
+
+        if (in_array($lines, $seen, true)) {
+            $startOfLoop = array_search($lines, $seen, true);
+            break;
+        }
+
+        $seen[$iterations] = $lines;
+    }
+
+    $seekIndex = (1000000000 - $startOfLoop) % ($iterations - $startOfLoop) + $startOfLoop;
+
+    if (! array_key_exists($seekIndex, $seen)) {
+        throw new \Exception('Seek index not found');
+    }
+
+    $total = 0;
+
+    foreach (array_reverse($seen[$seekIndex]) as $i => $column) {
+        $total += ($i + 1) * substr_count($column, 'O');
+    }
+
+    return $total;
+}
+
+check('2023 Day 14 Part 2 Example', '2023/inputs/day-14/part-2-example.txt', part2(...), 64);
+check('2023 Day 14 Part 2', '2023/inputs/day-14/input.txt', part2(...), 88680);
+produce('2023 Day 14 Part 2', '2023/inputs/day-14/input.txt', part2(...));
